04e106fc54bcb5d6854511759b1e77feb1cca7c3
Vivian Stewart <vivichrist@gmail.com>
2014-08-12 15:30:53 +1200

    remove package moves

--- src/Place.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Point;
-
-
-public interface Place
-{
-	// query whether is nearer to the destination a player wishes to travel
-	abstract boolean closer( Point target, Point source );
-}
