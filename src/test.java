523ef78a5faf62d53e8175a2f80810e810f57f48
Vivian Stewart <vivichrist@gmail.com>
2014-08-19 23:13:25 +1200

    test class by bo shen

--- src/test.java
+++ src/test.java
@@ -4,11 +4,10 @@ import identities.Cards;
 import game.Room;
 
 import org.junit.*;
+
 public class test {
 	@Test
 	public void test1(){
 		Room r1 = new Room(0, 0, Cards.BALLROOM, new Area() );
-
 	}
-
 }
